b7695ba8bf6421f4a94655e036a87574a0df936b
Fabrizio Montesi <famontesi@gmail.com>
2017-03-13 09:02:55 +0100

    New examples.

---
+++ lectures/thread_construction/src/cp/ThreadConstruction.java
@@ -0,0 +1,40 @@
+package cp;
+
+/**
+ *
+ * @author Fabrizio Montesi <fmontesi@imada.sdu.dk>
+ */
+public class ThreadConstruction
+{
+	private static class MyThread extends Thread {
+		public void run()
+		{
+			System.out.println( "Hello" );
+		}
+	}
+	
+	private static class MyRunnable implements Runnable {
+		public void run()
+		{
+			System.out.println( "Hello" );
+		}
+	}
+	
+	private static void myRunMethod()
+	{
+		System.out.println( "Hello" );
+	}
+	
+	public static void main( String[] args )
+	{
+		// All equivalent!
+		// Choice depends on code reuse and engineering considerations.
+		
+		new Thread( () -> {
+			System.out.println( "Hello" );
+		} ).start();
+		new MyThread().start();
+		new Thread( new MyRunnable() ).start();
+		new Thread( ThreadConstruction::myRunMethod ).start();
+	}
+}
